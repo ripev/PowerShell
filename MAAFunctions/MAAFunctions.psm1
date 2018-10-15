e42fcfaaef4a38eae98c0d811fc2f967e13e3333
Andrey Makovetsky <makovetskiy.a@zoloto585.ru>
2018-10-15 12:53:14 +0300

    .net versions

--- MAAFunctions/MAAFunctions.psm1
+++ MAAFunctions/MAAFunctions.psm1
@@ -594,3 +594,40 @@ function Select-ColorString {
 	end {
 	}
 }
+
+function Get-NetVersions {
+<#
+	.Synopsis
+		Get .net framework versions from Windows Registry
+	.Description
+		Get .net framework versions from Windows Registry
+	.Link
+		https://stackoverflow.com/questions/3487265/powershell-script-to-return-versions-of-net-framework-on-a-machine/3495491#3495491
+#>
+	$Lookup = @{
+		378389 = [version]'4.5'
+		378675 = [version]'4.5.1'
+		378758 = [version]'4.5.1'
+		379893 = [version]'4.5.2'
+		393295 = [version]'4.6'
+		393297 = [version]'4.6'
+		394254 = [version]'4.6.1'
+		394271 = [version]'4.6.1'
+		394802 = [version]'4.6.2'
+		394806 = [version]'4.6.2'
+		460798 = [version]'4.7'
+		460805 = [version]'4.7'
+		461308 = [version]'4.7.1'
+		461310 = [version]'4.7.1'
+		461808 = [version]'4.7.2'
+		461814 = [version]'4.7.2'
+	}
+
+	Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse |
+		Get-ItemProperty -name Version, Release -EA 0 |
+		# For One True framework (latest .NET 4x), change match to PSChildName -eq "Full":
+			Where-Object { $_.PSChildName -match '^(?!S)\p{L}'} |
+				Select-Object @{name = ".NET Framework"; expression = {$_.PSChildName}}, 
+				@{name = "Product"; expression = {$Lookup[$_.Release]}}, 
+				Version, Release
+}
