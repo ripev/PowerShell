99f8f59466d0331f87549019462ffbc7cbe85ca1
Andrey Makovetsky <makovetskiy.a@zoloto585.ru>
2018-10-18 14:18:52 +0300

    remove "output" from Modules

--- MAAPSModule/MAAPSModule.psm1
+++ MAAPSModule/MAAPSModule.psm1
@@ -1,45 +1,6 @@
 ﻿$MAAPSModulePSD1url = "https://raw.githubusercontent.com/ripev/PowerShell/master/MAAPSModule/MAAPSModule.psd1"
 $MAAPSModulePSM1url = "https://raw.githubusercontent.com/ripev/PowerShell/master/MAAPSModule/MAAPSModule.psm1"
 
-Function Output {
-<#
-	.SYNOPSIS
-		Output on screen (and file) some string
-
-	.DESCRIPTION
-		Write-Host and Write-Output in one flacon :)
-
-	.PARAMETER str
-		String to output
-
-	.PARAMETER outfile
-		File path to output
-
-	.PARAMETER color
-		Colorized output to display
-
-	.EXAMPLE
-		Output -str "Kalya-malya" -color Red
-
-		Output "Kalya-malya" on display with red color
-
-	.EXAMPLE
-		Output -str "Kalya-malya" -outfile .\output.txt
-
-		Output "Kalya-malya" on display with white color and to file .\output.txt
-
-	.LINK
-		https://github.com/ripev/PowerShell/
-#>
-	Param (
-		[string] $str,
-		[string] $outfile,
-		[ValidateSet("Black","DarkBlue","DarkGreen","DarkCyan","DarkRed","DarkMagenta","DarkYellow","Gray","DarkGray","Blue","Green","Cyan","Red","Magenta","Yellow","White")][string]$color="White"
-	)
-	if ($outfile) {Write-Output "$str" | Out-File "$outfile" -Encoding utf8 -Append}
-	Write-Host "$str" -f $color
-}
-
 Function Get-MAAPSModuleInternetVersion {
 <#
 	.SYNOPSIS
